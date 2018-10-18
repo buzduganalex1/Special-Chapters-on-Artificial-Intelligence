dc4b9d94c64ce31e97f582dcfaa8657f887e98b9
Alexandru <Alexandru@DESKTOP-C9OERBQ>
2018-10-18 23:42:51 +0300

    Small refactor

--- Homework1/Homework1-Exercise2.R
+++ Homework1/Homework1-Exercise2.R
@@ -1,21 +1,17 @@
-#2
-source("/Homework1/Functions.R")
+source("Functions.R")
+
 x <- c(1, 8, 2, 6, 3, 8, 8, 5, 5, 5)
-#a)
+
 sum(x)/10
 
-#b) 
 lapply(x, log2)
 
-#c) 
 max(x)-min(x)
-
-#d)
 
 computeY(x)
 
-#e)
-
 y <- computeY(x)
+
 mean(y)
+
 sd(y)
