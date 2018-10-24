1621f192612e4a12dd4db1c2f7e0a0f9a82ad571
Buzdugan\alexandru.buzdugan <buzduganalex1@gmail.com>
2018-10-24 17:28:31 +0300

    Added exercise 3 for homework 2

---
+++ Homework2/Exercise3.R
@@ -0,0 +1,16 @@
+u <- 0
+q <- c(0.1, 1, 10)
+
+range <- seq(-20, 20)
+
+require(graphics)
+
+x <- range
+
+## Using "log = TRUE" for an extended range :
+par(mfrow = c(2,1))
+plot(function(x) dnorm(x, log = TRUE), -60, 50,
+     main = "log { Normal density }")
+curve(log(dnorm(x)), add = TRUE, col = "red", lwd = 2)
+mtext("dnorm(x, log=TRUE)", adj = 0)
+mtext("log(dnorm(x))", col = "red", adj = 1)
